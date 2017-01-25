0acf7577790302d3683394714147f4fdfecb3abf
J. Kenzal Hunter, Sr <kenzal.hunter@gmail.com>
2017-01-25 14:18:39 -0500

    Simple Customization

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,6 +49,11 @@
                 font-size: 84px;
             }
 
+
+            .subtitle {
+                font-size: 36px;
+            }
+
             .links > a {
                 color: #636b6f;
                 padding: 0 25px;
@@ -79,15 +84,10 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Track
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="subtitle m-b-md">
+                    A Movie Tracking App
                 </div>
             </div>
         </div>
